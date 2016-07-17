71360ec44635f17096afb1b8068ac014f39418ff
Dylan Rush <dylan.t.rush0@gmail.com>
2016-07-17 13:15:14 -0600

    Added the program files for the project

---
+++ PockerGame Submission/PockerGame Submission/PockerGame/Pockergame/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    /// <summary>
+    /// Player class contains a hand for the game of cards they are playing
+    /// </summary>
+    class Player
+    {
+        private Hand playerHand; //keeps track of the users current hand
+        private string playerName;
+        int score; //keeps track of the current users score, # of times they have won during that game
+        /// <summary>
+        /// initilize the score for the player
+        /// </summary>
+        public Player(string playerName_,int maxNumberofCards_)
+        {
+            playerHand = new Hand(maxNumberofCards_);
+            score = 0;
+            playerName = playerName_;
+
+        }
+        /// <summary>
+        /// Sets the next availible card spot for the user
+        /// </summary>
+        /// <param name="new_card">A new card to add to the hand</param>
+        /// <returns>true if it cand add it, else false</returns>
+
+
+
+        public bool SetCard(Card new_card)
+        {
+            return playerHand.SetCard(new_card); 
+        }
+        /// <summary>
+        /// Incriments the players current score when then win
+        /// </summary>
+        public void IncrimentScore()
+        {
+            score++;
+        }
+        /// <summary>
+        /// Clears the current hand of this user
+        /// </summary>
+        public void Clear()
+        {
+            playerHand.Clear();
+        }
+        /// <summary>
+        /// Shows the hand of this user
+        /// </summary>
+        public void ShowHand()
+        {
+            Console.Write(playerName + " ");
+            playerHand.ShowCards();
+            Console.WriteLine("");
+        }
+    }
+
+}
---
+++ PockerGame Submission/PockerGame Submission/README.txt
@@ -0,0 +1,8 @@
+This folder is to contain the Project Pocker Chalange
+
+There are three versions, the first two, pockergame and pockergamev0.2 are only there to show my proccess in creating a correct solution
+
+the correct version is pockergamev0.3
+
+
+
